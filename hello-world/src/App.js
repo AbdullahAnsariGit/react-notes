ff4c3b380df260ceb7b92249a07b1e07c057c7b0
abdullahansarigit <abdullahbusiness11@gmail.com>
2025-02-22 13:07:29 +0500

    Class 05

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,94 +1,19 @@
-import React from "react";
-
-// const Header = () => {
-//   return (
-//     <div style={{ width: '70%', height: 70, background: 'green', display: 'flex', justifyContent: 'space-between', padding: 10 }}>
-//       <h1>Logo</h1>
-//       <h2>Title</h2>
-//     </div>
-//   )
-// }
-
-// const Content = () => {
-//   return (
-//     <div style={{ width: '70%', height: 70, background: 'red', display: 'flex', justifyContent: 'space-between', padding: 10 }}>
-//       <h1>Content</h1>
-//     </div>
-//   )
-// }
-
-// function Card() {
-//   return (
-//     <div className='card'>
-//       <h1>Mango</h1>
-//       <h2>Price: 120 rps</h2>
-//       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero molestiae, iure excepturi ea distinctio modi voluptate aut id dignissimos voluptatem dolores recusandae ipsum assumenda consequuntur suscipit! Veritatis quas eaque totam.</p>
-//     </div>
-//   )
-// }
-
-// const Card = () => {
-//  return (
-//     <div className='card'>
-//       <h1>Mango</h1>
-//       <h2>Price: 120 rps</h2>
-//       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero molestiae, iure excepturi ea distinctio modi voluptate aut id dignissimos voluptatem dolores recusandae ipsum assumenda consequuntur suscipit! Veritatis quas eaque totam.</p>
-//     </div>
-//   )
-// }
-
-import "./App.css";
-import { Greeting } from "./components/Card";
-import mango from './assets/mango.jpg'
-import apple from './assets/apple.jpg'
-
-
-
-
+import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './screens/Home'
+import Contact from './screens/Contact'
 function App() {
-  return (
-    <>
-      {/* Without using component */}
-      {/* <div className='card'>
-        <h1>Mango</h1>
-        <h2>Price: 120 rps</h2>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero molestiae, iure excepturi ea distinctio modi voluptate aut id dignissimos voluptatem dolores recusandae ipsum assumenda consequuntur suscipit! Veritatis quas eaque totam.</p>
-      </div>
-      <div className='card'>
-        <h1>Mango</h1>
-        <h2>Price: 120 rps</h2>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero molestiae, iure excepturi ea distinctio modi voluptate aut id dignissimos voluptatem dolores recusandae ipsum assumenda consequuntur suscipit! Veritatis quas eaque totam.</p>
-      </div>
-      <div className='card'>
-        <h1>Mango</h1>
-        <h2>Price: 120 rps</h2>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero molestiae, iure excepturi ea distinctio modi voluptate aut id dignissimos voluptatem dolores recusandae ipsum assumenda consequuntur suscipit! Veritatis quas eaque totam.</p>
-      </div>
-      <div className='card'>
-        <h1>Mango</h1>
-        <h2>Price: 120 rps</h2>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero molestiae, iure excepturi ea distinctio modi voluptate aut id dignissimos voluptatem dolores recusandae ipsum assumenda consequuntur suscipit! Veritatis quas eaque totam.</p>
-      </div>
-      <div className='card'>
-        <h1>Mango</h1>
-        <h2>Price: 120 rps</h2>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero molestiae, iure excepturi ea distinctio modi voluptate aut id dignissimos voluptatem dolores recusandae ipsum assumenda consequuntur suscipit! Veritatis quas eaque totam.</p>
-      </div> */}
-      {/* With using component */}
-
-      {/* <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card /> */}
-
-      {/* <GlobalCard tag="#abc" price="120 Rps" />
-      <GlobalCard name="Orange" price="80 Rps" description="Xyz" />
-      <GlobalCard name="Apple" price="80 Rps" description="Abc" imgUrl={apple} /> */}
-
-      <Greeting name="Alex  "/>
-    </>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />}>
+                    <Route index element={<Home />} />
+                    <Route path="contact" element={<Contact />} />
+                    {/* <Route path="*" element={<NoPage />} /> */}
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
-export default App;
+export default App
